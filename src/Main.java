e7298e843b2d74b65dfca2d22386daa2be05ddff
homework-2 <an_1704_na@inbox.ru>
2022-08-05 20:55:37 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        int clientOS = 1;
+        if (clientOS == 1) { System.out.println ("Установите приложение для андроид");
+        }
+        else  System.out.println ("Установите приложение для IOS");
+
 
     }
 }
