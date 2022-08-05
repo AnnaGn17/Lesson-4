9a57ed9e22ee87e813dd579ca45abd5d99111c02
homework-2 <an_1704_na@inbox.ru>
2022-08-05 21:56:12 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        int y = 2021;
+        if (y % 4 == 0 && y != 0 || y % 400 == 0) {System.out.println("год високосный");
+        } else {System.out.println("год не високосный");
+
+        }
+
     }
+
 }
