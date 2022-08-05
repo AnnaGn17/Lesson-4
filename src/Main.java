41ecd624efc682ae3e8c8650211d31d60cb0623f
homework-2 <an_1704_na@inbox.ru>
2022-08-05 22:33:06 +0300

    задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        int deliveryDisstance = 95;
+        int dayDisstance = deliveryDisstance / 30;
+        if (deliveryDisstance <= 20) {System.out.println ("1 день");
+
+        } else if ( dayDisstance > 0) {System.out.println (dayDisstance + " доставка");
+
+        }
+
     }
 }
